b8f071879736f5572108ca37d16abc585e8d6c98
Fergus Cooper <3770306+fcooper8472@users.noreply.github.com>
2024-06-28 15:36:28 +0200

    Update example to run with new entrypoints

--- run_example.sh
+++ run_example.sh
@@ -6,26 +6,29 @@ covarFile="example/covariates.txt"
 bgen="example/example.bgen"
 sample="example/example.sample"
 
+outDir="example/output"
+mkdir -p ${outDir}
+
 ## step 1: run model fitting (step 1) on genotypes and phenotypes
-python src/quickdraws_step1.py \
-   --out output/qd \
+quickdraws-step-1 \
+   --out ${outDir}/qd \
    --bed ${bed} \
    --phenoFile ${phenoFile} \
    --covarFile ${covarFile}
 
-## step 2: get association stats for SNPs in bgen file
-python src/quickdraws_step2.py \
-    --out output/qd \
+# step 2: get association stats for SNPs in bgen file
+quickdraws-step-2 \
+    --out ${outDir}/qd \
     --bed ${bed} \
-    --out_step1 output/qd \
+    --out_step1 ${outDir}/qd \
     --covarFile ${covarFile} \
     --unrel_sample_list example/unrelated_FID_IID.txt
 
-python src/quickdraws_step2.py \
-    --out output/qd_imputed \
+quickdraws-step-2 \
+    --out ${outDir}/qd_imputed \
     --bgen ${bgen} \
     --sample ${sample} \
-    --out_step1 output/qd \
-    --calibrationFile output/qd.calibration \
+    --out_step1 ${outDir}/qd \
+    --calibrationFile ${outDir}/qd.calibration \
     --covarFile ${covarFile} \
     --unrel_sample_list example/unrelated_FID_IID.txt
