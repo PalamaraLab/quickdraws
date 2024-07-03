2609b9269c2136f7dfebd38e923a705f8fa4fbaa
Fergus Cooper <3770306+fcooper8472@users.noreply.github.com>
2024-07-03 13:41:02 +0200

    Fix example_bin script

--- run_example_bin.sh
+++ run_example_bin.sh
@@ -6,9 +6,12 @@ covarFile="example/covariates.txt"
 bgen="example/example.bgen"
 sample="example/example.sample"
 
+outDir="example/output"
+mkdir -p ${outDir}
+
 ## step 1: run model fitting (step 1) on genotypes and phenotypes
-python src/quickdraws_step1.py \
-   --out output/qd_bin \
+quickdraws-step-1 \
+   --out ${outDir}/qd_bin \
    --bed ${bed} \
    --phenoFile ${phenoFile} \
    --covarFile ${covarFile} \
@@ -16,21 +19,21 @@ python src/quickdraws_step1.py \
    --binary
 
 ## step 2: get association stats for SNPs in bgen file
-python src/quickdraws_step2.py \
-    --out output/qd_bin \
+quickdraws-step-2 \
+    --out ${outDir}/qd_bin \
     --bed ${bed} \
-    --out_step1 output/qd_bin \
+    --out_step1 ${outDir}/qd_bin \
     --covarFile ${covarFile} \
     --unrel_sample_list example/unrelated_FID_IID.txt \
     --binary \
     --firth
 
-python src/quickdraws_step2.py \
-    --out output/qd_bin_imputed \
+quickdraws-step-2 \
+    --out ${outDir}/qd_bin_imputed \
     --bgen ${bgen} \
     --sample ${sample} \
-    --out_step1 output/qd_bin \
-    --calibrationFile output/qd_bin.calibration \
+    --out_step1 ${outDir}/qd_bin \
+    --calibrationFile ${outDir}/qd_bin.calibration \
     --covarFile ${covarFile} \
     --unrel_sample_list example/unrelated_FID_IID.txt \
     --binary \
