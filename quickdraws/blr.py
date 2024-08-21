4c5af5b09af7e2e09109ccf81240f11afdbbf64f
Hrushikesh Loya <vnk166@rescomp2.hpc.in.bmrc.ox.ac.uk>
2024-08-21 22:02:39 +0100

    bug fix

--- quickdraws/blr.py
+++ quickdraws/blr.py
@@ -50,7 +50,6 @@ from .correct_for_relatives import get_correction_for_relatives
 from .scripts import get_cpu_count
 
 logger = logging.getLogger(__name__)
-torch._dynamo.config.cache_size_limit = 128
 
 if torch.cuda.is_available():
     import bitsandbytes as bnb
